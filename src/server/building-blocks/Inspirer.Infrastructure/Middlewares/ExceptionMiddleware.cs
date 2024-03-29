3ac9b698d1b5f1289ef39f40cb76f8f2f151d482
danyducky <morikov2000@gmail.com>
2024-03-29 19:47:48 +0300

    Add application exceptions TASK-33

--- src/server/building-blocks/Inspirer.Infrastructure/Middlewares/ExceptionMiddleware.cs
+++ src/server/building-blocks/Inspirer.Infrastructure/Middlewares/ExceptionMiddleware.cs
@@ -1,4 +1,9 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Inspirer.Application.Exceptions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace Inspirer.Infrastructure.Middlewares;
 
@@ -7,12 +12,30 @@ namespace Inspirer.Infrastructure.Middlewares;
 /// </summary>
 public class ExceptionMiddleware : IMiddleware
 {
+    private const string MimeType = "application/problem+json";
+
+    private static readonly Dictionary<Type, int> ExceptionStatusCodes = new()
+    {
+        { typeof(ValidationException), StatusCodes.Status400BadRequest },
+        { typeof(UnauthorizedException), StatusCodes.Status401Unauthorized },
+        { typeof(ForbiddenException), StatusCodes.Status403Forbidden },
+        { typeof(NotFoundException), StatusCodes.Status404NotFound },
+    };
+
+    private static readonly JsonSerializerOptions Options = new(JsonSerializerDefaults.Web)
+    {
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
+
+    private readonly IHostEnvironment environment;
+
     /// <summary>
     /// Constructor.
     /// </summary>
-    public ExceptionMiddleware()
+    /// <param name="environment">Application host environment.</param>
+    public ExceptionMiddleware(IHostEnvironment environment)
     {
-
+        this.environment = environment;
     }
 
     /// <inheritdoc />
@@ -22,9 +45,52 @@ public class ExceptionMiddleware : IMiddleware
         {
             await next(context);
         }
-        catch (Exception ex)
+        catch (Exception exception)
         {
-            // Handle an application exception and unify a response message.
+            if (context.Response.HasStarted)
+            {
+                throw;
+            }
+
+            var details = GetProblemDetails(context, exception);
+            var json = JsonSerializer.Serialize(details, Options);
+
+            context.Response.ContentType = MimeType;
+
+            await context.Response.WriteAsync(json);
         }
     }
+
+    private ProblemDetails GetProblemDetails(HttpContext context, Exception exception)
+    {
+        var details = new ProblemDetails
+        {
+            Title = GetExceptionTitle(exception),
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.ToString(),
+        };
+
+        if (ExceptionStatusCodes.TryGetValue(exception.GetType(), out var statusCode))
+        {
+            details.Status = statusCode;
+        }
+
+        if (environment.IsProduction())
+        {
+            return details;
+        }
+
+        details.Extensions["traceId"] = context.TraceIdentifier;
+        details.Extensions["data"] = exception.Data;
+
+        return details;
+    }
+
+    private static string GetExceptionTitle(Exception exception) => exception switch
+    {
+        UnauthorizedException => "Unauthorized",
+        ForbiddenException => "Forbidden",
+        NotFoundException => "Not Found",
+        _ => "Internal server error",
+    };
 }
